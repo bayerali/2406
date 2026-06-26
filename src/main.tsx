787e3424e291de8b30c0c58201f7de3a53613955
bayerali <ali.ismail.ext@bayer.com>
2026-06-26 02:01:23 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
-import "./styles.css";
+import "./styles/globals.css";
 
 declare global {
   interface Window {
